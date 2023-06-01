4a079c99c2e9033a5537600e8bfce8228c769244
vuongquocnguyen <nguvuong@sheridancollege.ca>
2023-06-01 07:21:26 -0400

    first commit

---
+++ src/javapractice/JavaPractice.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javapractice;
+import java.util.Arrays;
+/**
+ *
+ * @author nguye
+ */
+
+
+public class JavaPractice{
+    public static void main(String[] args){        
+        char[] newArr = "Hello World".toCharArray();
+        char[] inversedArr = new char[newArr.length];
+        for (int i =0; i < newArr.length; i++){
+            inversedArr[i] = newArr[newArr.length -1 -i];
+        }
+        String result = new String(inversedArr);
+        System.out.println(result);
+       
+        
+        
+    }
+    
+   
+    
+    
+    
+}
